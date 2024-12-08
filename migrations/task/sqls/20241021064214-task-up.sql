dbedc5b086dd02fe173784d9caaf8d2367fa24a3
dardijames <dardijames@gmail.com>
2024-12-08 20:03:07 +0800

    修改v1.4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -155,16 +155,15 @@ DELETE FROM "SKILL" WHERE name = '空中瑜珈';
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
 
 INSERT INTO "COURSE"(user_id, skill_id, name, start_at, end_at, max_participants, meeting_url)
-VALUES
- (
-	(SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'),
+VALUES (
+  (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'),
   (SELECT id FROM "SKILL" WHERE name = '重訓'),
   '重訓基礎課',
   '2024-11-25 14:00:00',
   '2024-11-25 16:00:00',
-  '10',
+  10,
   'https://test-meeting.test.io'
- );
+);
 
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
