56b7cada449018e06ea4a61687c8e385edb82b14
dardijames <dardijames@gmail.com>
2024-12-09 21:00:38 +0800

    6.4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -328,12 +328,3 @@ WHERE "CREDIT_PURCHASE".purchase_at >= '2024-11-01 00:00:00'
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
-
-SELECT 
-  COUNT(DISTINCT ("COURSE_BOOKING".user_id)) AS "預約會員人數"
-FROM 
-  "COURSE_BOOKING"
-WHERE 
-  "COURSE_BOOKING".created_at >= '2024-11-01 00:00:00'
-  AND "COURSE_BOOKING".created_at <= '2024-11-30 23:59:59'
-  AND "COURSE_BOOKING".status != '課程已取消';
